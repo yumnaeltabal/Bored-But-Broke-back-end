986a6f4fbbe418dcdbbaa099dc4ff2389ae50fcf
yumna <yumnaeltaba@gmail.com>
2026-05-28 22:46:54 +0100

    created weather result class

---
+++ Bored-But-Broke-back-end/Models/WeatherResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Bored_But_Broke_back_end.Models
+{
+    public class WeatherResult
+    {
+        public string City { get; set; } = "";
+        public DateTime DateTime { get; set; }
+        public double Temperature { get; set; }
+    }
+}
