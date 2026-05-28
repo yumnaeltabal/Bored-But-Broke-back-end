bc6e75ac536a27d8fdd229ef81740dcaf0eea7ef
yumna <yumnaeltaba@gmail.com>
2026-05-28 22:33:04 +0100

    class to translate for JSON

---
+++ Bored-But-Broke-back-end/Models/GeoResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Bored_But_Broke_back_end.Models
+{
+    public class GeoResponse
+    {
+        public GeoResult[] result { get; set; }
+    }
+    public class GeoResult
+    {
+        public string name { get; set; } = "";
+        public string latitude { get; set; } = "";
+        public string longitude { get; set; } = "";
+        public string country { get; set; } = "";
+    }
+}
