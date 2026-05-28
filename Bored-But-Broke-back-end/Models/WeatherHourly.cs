e5c5bb23b4339c5a83e195332b9ef3b84c0e747f
yumna <yumnaeltaba@gmail.com>
2026-05-28 23:01:25 +0100

    created the weather hourly class for specific times

---
+++ Bored-But-Broke-back-end/Models/WeatherHourly.cs
@@ -0,0 +1,13 @@
+﻿namespace Bored_But_Broke_back_end.Models
+{
+    public class WeatherHourly
+    {
+        public HourlyData hourly { get; set; } = default!;
+    }
+
+    public class HourlyData 
+    { 
+        public string[] time { get; set; } = []; 
+        public double[] temperature_2m { get; set; } = []; 
+    }
+}
