59d84ed40709f471a89163c0e5cf8bfcde473d80
yumna <yumnaeltaba@gmail.com>
2026-05-29 16:49:46 +0100

    created endpoint and swagger post visable

--- Bored-But-Broke-back-end/Program.cs
+++ Bored-But-Broke-back-end/Program.cs
@@ -1,3 +1,7 @@
+using Bored_But_Broke_back_end.ExternalAPI.WeatherApi;
+using Bored_But_Broke_back_end.Models;
+using Bored_But_Broke_back_end.Services;
+
 namespace Bored_But_Broke_back_end
 {
     public class Program
@@ -6,12 +10,23 @@ namespace Bored_But_Broke_back_end
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddHttpClient<IWeatherClient, WeatherClient>();
+            builder.Services.AddScoped<IWeatherService, WeatherService>();
+
             builder.Services.AddControllers();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.MapPost("/weather", async (
+                WeatherRequest request,
+                IWeatherService service) =>
+            {
+                var result = await service.GetWeatherAndForwardAsync(request);
+                return Results.Ok(result);
+            });
 
             if (app.Environment.IsDevelopment())
             {
