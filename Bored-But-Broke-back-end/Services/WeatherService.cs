4d9cfe95935c15a1f33fcedb522562cb1e6392a0
yumna <yumnaeltaba@gmail.com>
2026-05-29 16:54:27 +0100

    restructured services so it can be optimal with the clientside

--- Bored-But-Broke-back-end/Services/WeatherService.cs
+++ Bored-But-Broke-back-end/Services/WeatherService.cs
@@ -1,4 +1,7 @@
-﻿using Bored_But_Broke_back_end.Models;
+﻿using Bored_But_Broke_back_end.ExternalAPI.WeatherApi;
+using Bored_But_Broke_back_end.Models;
+using Bored_But_Broke_back_end.Services;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -7,56 +10,42 @@ namespace Bored_But_Broke_back_end.Services
 {
     public class WeatherService : IWeatherService
     {
-       private readonly HttpClient _httpClient;
-        public WeatherService(HttpClient httpClient)
+        private readonly IWeatherClient _client;
+        public WeatherService(IWeatherClient client)
         {
-            _httpClient = httpClient;
+            _client = client;
         }
 
-        public async Task<WeatherResult> GetWeatherAndForwardAsync(WeatherRequest request)
+        public async Task<List<WeatherHourlyResult>> GetWeatherAndForwardAsync(WeatherRequest request)
         {
-            Console.WriteLine("WEATHER SERVICE BEGINNING");
 
-            Console.WriteLine("Frontend request received:");
-            Console.WriteLine($"city: {request.City}");
-            Console.WriteLine($"date: {request.Date}");
-            Console.WriteLine($"time: {request.Time}");
+            var weather = await _client.GetWeatherAsync(request.lat,
+                                                        request.lon,
+                                                        request.Date);
 
-            var dateTime = DateTime.Parse($"{request.Date} {request.Time}");
-            Console.WriteLine($"parsed DateTime: {dateTime}");
+            if (weather == null || weather.hourly == null)
+                throw new Exception("Weather API failed");
 
-            var coordConverter = $"https://geocoding-api.open-meteo.com/v1/search?name={request.City}&count=1";
-            Console.WriteLine("Calling Geocoding API...");
-            Console.WriteLine($"geocoding URL: {coordConverter}");
+            var results = new List<WeatherHourlyResult>();
 
-            var coord = await _httpClient.GetFromJsonAsync<GeoResponse>(coordConverter);
-
-            if (coord?.results == null || coord.results.Length == 0)
+            foreach (var hour in request.Hours)
             {
-                Console.WriteLine("ERROR: City not found");
-                throw new Exception("City not found");
+                var targetTime = $"{request.Date}T{hour:D2}:00";
+                var index = Array.IndexOf(weather.hourly.time, targetTime);
+
+                if (index == -1)
+                {
+                    continue;
+                }
+                results.Add(new WeatherHourlyResult
+                {
+                    Time = targetTime,
+                    Temperature = weather.hourly.temperature_2m[index],
+                    Precipitation = weather.hourly.precipitation[index],
+                    WindGust = weather.hourly.wind_gusts_10m[index]
+                });
             }
-
-            double lat = coord!.results[0].latitude;
-            double lon = coord.results[0].longitude;
-
-            var weatherUrl = $"https://api.open-meteo.com/v1/forecast" +
-                             $"?latitude={lat}" +
-                             $"&longitude={lon}" +
-                             $"&hourly=temperature_2m" +
-                             $"&start_date={dateTime:yyyy-MM-dd}" +
-                             $"&end_date={dateTime:yyyy-MM-dd}";
-
-            Console.WriteLine("Calling Weather API...");
-            Console.WriteLine($"Weather URL: {weatherUrl}");
-
-            var weather =
-            await _httpClient.GetFromJsonAsync<WeatherHourly>(weatherUrl);
-            if (weather == null)
-            {
-                Console.WriteLine("weather API failed");
-                throw new Exception("weather API failed");
-            }
+            return results;
         }
     }
 }
