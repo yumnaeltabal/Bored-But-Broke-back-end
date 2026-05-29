507bd2368e4ce2660138bf7a9aba00998af6d133
yumna <yumnaeltaba@gmail.com>
2026-05-29 15:45:55 +0100

    refactored from the WeatherService for organisation

--- Bored-But-Broke-back-end/ExternalAPI/WeatherApi/WeatherClient.cs
+++ Bored-But-Broke-back-end/ExternalAPI/WeatherApi/WeatherClient.cs
@@ -1,11 +1,36 @@
-﻿namespace Bored_But_Broke_back_end.ExternalAPI.WeatherApi
+﻿using Bored_But_Broke_back_end.Models;
+
+namespace Bored_But_Broke_back_end.ExternalAPI.WeatherApi
 {
     public interface IWeatherClient
     {
+        Task<OpenMetroResponse?> GetWeatherAsync(
+        double latitude,
+        double longitude,
+        string date);
     }
-    public class WeatherClient : IWeatherClient 
+    public class WeatherClient : IWeatherClient
     {
+        private readonly HttpClient _httpClient;
+
+        public WeatherClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<OpenMetroResponse?> GetWeatherAsync(double latitude, double longitude, string date)
+        {
+            var url =
+            $"https://api.open-meteo.com/v1/forecast" +
+            $"?latitude={latitude}" +
+            $"&longitude={longitude}" +
+            $"&hourly=temperature_2m,precipitation,wind_gusts_10m" +
+            $"&start_date={date}" +
+            $"&end_date={date}";
+
+            Console.WriteLine($"[OpenMeteoClient] {url}");
+
+            return await _httpClient.GetFromJsonAsync<OpenMetroResponse>(url);
+        }
 
     }
-
 }
