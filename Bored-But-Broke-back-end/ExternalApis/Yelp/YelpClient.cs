fd695235f314c591ebbf9f16e02e758d93ec71f9
yumna <yumnaeltaba@gmail.com>
2026-06-02 12:03:00 +0100

    made the YelpCLient GetPlaceById and added it to the YelpClient interface

--- Bored-But-Broke-back-end/ExternalApis/Yelp/YelpClient.cs
+++ Bored-But-Broke-back-end/ExternalApis/Yelp/YelpClient.cs
@@ -9,6 +9,7 @@ namespace Bored_But_Broke_back_end.ExternalApis.Yelp
     public interface IYelpClient
     {
         public Task<SearchResponse> BusinessesSearchAsync(Dictionary<string, StringValues> query, CancellationToken token);
+        public Task<YelpBusiness> BusinessesGetByIdAsync(string placeId, CancellationToken token);
     }
 
     // TODO: rewrite console log to logger
@@ -62,5 +63,41 @@ namespace Bored_But_Broke_back_end.ExternalApis.Yelp
                     errorDetail: "Failed to deserialize response from Yelp API."
                 );
         }
-    }
+        public async Task<YelpBusiness> GetBusinessByIdAsync(string placeId, CancellationToken token)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                response = await _httpClient.GetAsync($"businesses/{placeId}", token);
+                Console.WriteLine(response);
+            }
+
+            catch (HttpRequestException ex)
+            {
+                throw new HttpRequestException("Couldn't connect to Yelp API.", ex);
+            }
+
+            catch (TaskCanceledException ex)
+            {
+                throw new TaskCanceledException("Yelp API failed to respond in time.", ex);
+            }
+
+            string responseBody = await response.Content.ReadAsStringAsync(token);
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = JsonSerializer.Deserialize<ErrorResponse>(responseBody);
+                throw new ExternalApiException(
+                    statusCode: response.StatusCode,
+                    errorTitle: $"Yelp API error: {response.ReasonPhrase}",
+                    errorDetail: error?.Error?.Description
+                );
+            }
+            return JsonSerializer.Deserialize<YelpBusiness>(responseBody)
+                ?? throw new ExternalApiException(
+                    statusCode: HttpStatusCode.BadGateway,
+                    errorTitle: "Bad Gateway",
+                    errorDetail: "Failed to deserialize response from Yelp API."
+                );
+        }
 }
